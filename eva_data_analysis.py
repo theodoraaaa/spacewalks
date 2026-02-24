163f329a0f6a49b18875de03df13a8b213a6ca7b
Theodora Commandeur <theacommandeur@icloud.com>
2026-02-24 14:59:37 +0000

    renamed variables for clarity

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,41 +4,40 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('./eva-data.json', 'r', encoding = 'ascii')
-data_t = open('./eva-data.csv', 'w', encoding = 'utf-8')
-g_file = './cumulative_eva_graph.png'
+input_file = open('./eva-data.json', 'r', encoding = 'ascii')
+output_file = open('./eva-data.csv', 'w', encoding = 'utf-8')
+graph_file = './cumulative_eva_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
 data=[]
 
 for i in range(375):
-    line=data_f.readline()
+    line=input_file.readline()
     print(line)
     data.append(json.loads(line[1:-1]))
 #data.pop(0)
 ## Comment out this bit if you don't want the spreadsheet
 
-w=csv.writer(data_t)
-
+csv_writer = csv.writer(output_file)
 
 time = []
-date =[]
+date = []
 
 j=0
 for i in data:
     print(data[j])
     # and this bit
-    w.writerow(data[j].values())
+    csv_writer.writerow(data[j].values())
     if 'duration' in data[j].keys():
-        tt=data[j]['duration']
-        if tt == '':
+        duration_str=data[j]['duration']
+        if duration_str == '':
             pass
         else:
-            t=dt.datetime.strptime(tt,'%H:%M')
-            ttt = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()/(60*60)
-            print(t,ttt)
-            time.append(ttt)
+            duration_dt=dt.datetime.strptime(duration_str,'%H:%M')
+            duration_hrs = dt.timedelta(hours=duration_dt.hour, minutes=duration_dt.minute, seconds=duration_dt.second).total_seconds()/(60*60)
+            print(duration_dt,duration_hrs)
+            time.append(duration_hrs)
             if 'date' in data[j].keys():
                 date.append(dt.datetime.strptime(data[j]['date'][0:10], '%Y-%m-%d'))
                 #date.append(data[j]['date'][0:10])
@@ -47,17 +46,17 @@ for i in data:
                 time.pop(0)
     j+=1
 
-t=[0]
+duration_dt=[0]
 for i in time:
-    t.append(t[-1]+i)
+    duration_dt.append(duration_dt[-1]+i)
 
 date,time = zip(*sorted(zip(date, time)))
 
 
 
-plt.plot(date,t[1:], 'ko-')
+plt.plot(date,duration_dt[1:], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
-plt.savefig(g_file)
+plt.savefig(graph_file)
 plt.show()
